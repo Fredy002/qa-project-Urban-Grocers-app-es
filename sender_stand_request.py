009bab7d4b05313ec8cb0874d85d27440444a242
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-09-26 12:51:01 -0500

    add post_new_kit function for creating a new kit

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -2,16 +2,34 @@ import configuration
 import requests
 import data
 
+def get_headers(auth_token=None):
+    headers = data.headers.copy()
+    if auth_token:
+        headers["Authorization"] = f"Bearer {auth_token}"
+    return headers
+
+
 def post_new_user(body):
+    headers = get_headers()
     return requests.post(
         configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
-        json=body, headers=data.headers
+        json=body,
+        headers=headers
     )
 
+
 def get_kits_table(auth_token):
-    headers = data.headers.copy()
+    headers = get_headers(auth_token)
+    return requests.get(
+        configuration.URL_SERVICE + configuration.KITS_PATH,
+        headers=headers
+    )
 
-    #"Authorization": "Bearer jknnFApafP4awfAIFfafam2fma"
-    headers["Authorization"] = f"Bearer {auth_token}"
 
-    return requests.get(configuration.URL_SERVICE + configuration.KITS_PATH, headers=headers)
+def post_new_kit(kit_body, auth_token):
+    headers = get_headers(auth_token)
+    return requests.post(
+        configuration.URL_SERVICE + configuration.KITS_PATH,
+        json=kit_body,
+        headers=headers
+    )
